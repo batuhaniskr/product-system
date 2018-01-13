86303d9d81e1d219fcab2b13ab2644fc783489c8
Batuhan Işıker <batuhan.isiker@metglobal.com>
2018-01-13 12:30:33 +0300

    Added mvnw file.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@ComponentScan(basePackages = "com.repository")
 public class Application {
 
     public static void main(String[] args){
