cee117c02b2d803ca038bbce51220d89ef0bd4c4
Batuhan Işıker <batuhan.isiker@metglobal.com>
2018-01-17 14:08:13 +0300

    Added application test

---
+++ src/test/java/com/ApplicationTest.java
@@ -0,0 +1,45 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest {
+
+   @Autowired
+   private MockMvc mockMvc;
+
+   @Test
+   public void loginWithValidUserThenAuthenticated() throws Exception{
+
+      SecurityMockMvcRequestBuilders.FormLoginRequestBuilder formLoginRequestBuilder = formLogin()
+              .user("user")
+              .password("user");
+
+      mockMvc.perform(formLoginRequestBuilder)
+              .andExpect(authenticated().withUsername("user"));
+   }
+
+   @Test
+   public void loginInvalidUserThenUnauthenticated() throws Exception{
+
+      SecurityMockMvcRequestBuilders.FormLoginRequestBuilder formLoginRequestBuilder = formLogin()
+              .user("user")
+              .password("wrong_password");
+
+      mockMvc.perform(formLoginRequestBuilder)
+              .andExpect(unauthenticated());
+   }
+}
