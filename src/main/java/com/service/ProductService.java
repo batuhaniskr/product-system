15467fb4a7f0fde0745e011c1f1dcc7f67e9890a
Batuhan Işıker <bthnskr@gmail.com>
2018-07-09 16:08:01 +0300

    Added category table on product system.

--- src/main/java/com/service/ProductService.java
+++ src/main/java/com/service/ProductService.java
@@ -1,31 +1,34 @@
 package com.service;
 
+import com.model.Category;
 import com.model.Product;
+import com.repository.CategoryRepository;
 import com.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class ProductService {
 
     private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Page<Product> getAllProduct(Pageable pageable) {
-
         return productRepository.findAll(pageable);
     }
 
-    public void saveProduct(Product product){
+    public void saveProduct(Product product) {
+        Category category = categoryRepository.findByCategoryName(product.getCategory().getCategoryName());
+        product.setCategory(category);
         productRepository.save(product);
     }
 
@@ -33,7 +36,7 @@ public class ProductService {
         productRepository.delete(id);
     }
 
-    public Product getProductById(Integer id){
+    public Product getProductById(Integer id) {
         return productRepository.findOne(id);
     }
 }
