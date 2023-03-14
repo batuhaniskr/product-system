b942e34a051b8fea9291b6bbc1a83115ffb93172
batuhaniskr <batuhaniskr@gmail.com>
2023-03-14 14:55:00 +0300

    Added security unit tests for MainController

--- src/main/java/com/batuhaniskr/product/controller/MainController.java
+++ src/main/java/com/batuhaniskr/product/controller/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
